9e1a0b412b989cbfc3f333340cf143bfd235680e
Byte Yue <yj976240184@gmail.com>
2020-12-15 15:44:05 +0800

    修复发送的json数据有误

--- server/expression/filehandler.go
+++ server/expression/filehandler.go
@@ -1,10 +1,5 @@
 package expression
 
-import (
-	"encoding/json"
-	"fmt"
-)
-
 type ExGroup struct {
 	Exp Ex  `json:"exp"` //注意要大写才能被外界访问
 	ID  int `json:"id"`
@@ -20,7 +15,7 @@ type ExGroup struct {
 
 //将运算式生成json数据
 //times:用户第几次使用,name:用户名 用户一同生成文件名
-func GenerateWholeJsonExpression(times int, name string, allExpression []Ex) []byte {
+func GenerateWholeJsonExpression(times int, name string, allExpression []Ex) map[string]interface{} {
 	js := make(map[string]interface{})
 	jsons := make([]interface{}, 0)
 	for flag := 0; flag < len(allExpression); flag++ {
@@ -35,14 +30,9 @@ func GenerateWholeJsonExpression(times int, name string, allExpression []Ex) []b
 		jsons = append(jsons, temp)
 	}
 	js["expressions"] = jsons
-	//println(jsons)
-	bt, errs := json.Marshal(js)
-	if errs != nil {
-		fmt.Println("json marshal error:", errs)
-	}
-	//err=
-	return bt
-	//fmt.Println(string(bt))
-	//fmt.Print(len(jsons))
+	//bt, errs := json.Marshal(js)
+	//if errs != nil {
+	//	fmt.Println("json marshal error:", errs)
+	//}
+	return js
 }
-
